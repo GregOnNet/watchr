2b9a163726e93bb88b1cdbc28c53e7979954268d
Alexander Groß <agross@therightstuff.de>
2020-05-10 16:08:38 +0200

    Fix up VS crap

--- source/Web/BundleConfig.cs
+++ source/Web/BundleConfig.cs
@@ -9,9 +9,9 @@ namespace Web
       bundles.Add(new StyleBundle("~/assets/css")
                     .Include("~/Css/style.css"));
 
-      bundles.Add(new ScriptBundle("~/assets/js/app")
+      bundles.Add(new ScriptBundle("~/assets/js")
                     .Include("~/Scripts/lib/signalr/server.js")
-                    .Include("~/Scripts/index.js")
+                    .Include("~/Scripts/index.min.js")
                     .ForceOrdered());
     }
   }
